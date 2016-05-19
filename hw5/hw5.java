4d334c309e576b4a22e6dd102bfa0ffe438957d6
Frank Chen <frankchen1698@gmail.com>
2016-05-19 01:10:51 -0700

    fixed mirror2

--- hw5/hw5.java
+++ hw5/hw5.java
@@ -10,6 +10,8 @@
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.*;
 
 // a marker for code that you need to implement
 class ImplementMe extends RuntimeException {}
@@ -102,6 +104,19 @@ class PPMImage {
 		os.close();
     }
 
+    public void swap(RGB p1, RGB p2) {
+		RGB tmp = new RGB(0, 0, 0);
+		tmp.R = p1.R;
+		tmp.G = p1.G;
+		tmp.B = p1.B;
+		p1.R = p2.R;
+		p1.G = p2.G;
+		p1.B = p2.B;
+		p2.R = tmp.R;
+		p2.G = tmp.G;
+		p2.B = tmp.B;
+	}
+
 	// implement using Java 8 Streams
     public PPMImage negate() {
     	// set wt, ht, mt
@@ -159,14 +174,94 @@ class PPMImage {
     	return new PPMImage(wt, ht, mt, p);
     }    
     
+    class MirrorTask extends RecursiveTask <RGB[]> {
+		private RGB[] px;
+		private int low, high;
+		private int width, height;
+
+		// constants are final - assigned once
+		private final int SEQUENTIAL_CUTOFF = 800;
+
+		public MirrorTask(RGB[] p, int low, int high, int width, int height) {
+			this.px = p;
+			this.low = low;
+			this.high = high;
+			this.width = width;
+			this.height = height;
+		}
+
+		public RGB[] compute() {
+			if (high - low > SEQUENTIAL_CUTOFF) {
+				int mid = (height / 2) * width;
+				MirrorTask left = 
+					new MirrorTask(px, low, mid, width, height/2);
+				MirrorTask right = 
+					new MirrorTask(px, mid, high, width, height/2);
+				left.fork(); // fork them both
+				// RGB[] l2 = right.compute(); // join is synchronization
+				// RGB[] l1 = left.join();
+				right.fork();
+				RGB[] l1 = left.join();
+				RGB[] l2 = right.join();
+				RGB[] comb = 
+					Stream.concat(Arrays.stream(l1), Arrays.stream(l2))
+						.toArray(RGB[]::new);
+                return comb;
+			} else {
+				for (int i = low; i < high; i++) {
+					int row = i / width + 1;
+					if (i < high - width/2)
+						swap(px[i], px[high-(i-(row-1)*width)-1]);
+				}
+				return Arrays.copyOfRange(px, low, high);
+			}
+		}
+	}
+
 	// implement using Java's Fork/Join library
     public PPMImage mirrorImage() {
-		throw new ImplementMe();
+		// set wt, ht, mt
+    	int wt = this.width;
+    	int ht = this.height;
+    	int mt = this.maxColorVal;
+    	// avoid pass by reference error - go through and copy RGB individually
+    	RGB[] px = Arrays.stream(pixels)
+    		.parallel()
+    		.map(pi -> new RGB(pi.R, pi.G, pi.B))
+    		.toArray(RGB[]::new);
+    	// use fork / join to create the new RGB pixels array p
+    	RGB[] p = new MirrorTask(px, 0, wt*ht, wt, ht).compute();
+    	return new PPMImage(wt, ht, mt, p);
     }
 
 	// implement using Java 8 Streams
     public PPMImage mirrorImage2() {
-		throw new ImplementMe();
+		// set wt, ht, mt
+    	int wt = this.width;
+    	int ht = this.height;
+    	int mt = this.maxColorVal;
+    	// avoid pass by reference error - go through and copy RGB individually
+    	RGB[] px = Arrays.stream(pixels)
+    		.parallel()
+    		.map(pi -> new RGB(pi.R, pi.G, pi.B))
+    		.toArray(RGB[]::new);
+    	// use .stream() on p, an RGB array
+    	// use .parallel() to parallelize
+    	// use .map to do computation
+    	// use .toArray(RGB[]::new) to convert back to RGB array
+    	int[] indexes = IntStream.range(0, wt*ht).toArray();
+    	Arrays.stream(indexes)
+    		.parallel()
+    		.forEach((id) -> {
+    			// extract current row number of id
+    			int row = (id / wt) + 1;
+    			// System.out.println("multiplier: " + (row*wt));
+    			// only swap when id is the left side of current row
+    			if (id < (row*wt - wt/2)) {
+    				swap(px[id], px[row*wt-(id-(row-1)*wt)-1]);
+    			}
+    		});
+    	return new PPMImage(wt, ht, mt, px);
     }
 
 	// implement using Java's Fork/Join library
@@ -212,9 +307,12 @@ class Test {
 			PPMImage img1 = new PPMImage(s1);
 			PPMImage neg_img1 = img1.negate();
 			PPMImage gray_img1 = img1.greyscale();
-			img1.toFile("original.ppm");
-			neg_img1.toFile("neg.ppm");
-			gray_img1.toFile("gray.ppm");
+			PPMImage mirror_img1_1 = img1.mirrorImage();
+			PPMImage mirror_img1_2 = img1.mirrorImage2();
+			img1.toFile("original_1.ppm");
+			neg_img1.toFile("neg_1.ppm");
+			mirror_img1_1.toFile("mirror_1_1.ppm");
+			mirror_img1_2.toFile("mirror_1_2.ppm");
 		} catch(FileNotFoundException e1) {
 			System.out.println("ERROR: can't find file.");
 		} catch(IOException e2) {
